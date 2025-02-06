b7b3685d47c5818f0dfa9b29feb14ae887aa8289
aleduc <arnaud.leduc@keyconsulting.fr>
2025-02-06 14:44:44 +0100

    Update frontend fix css

--- frontend/src/MarkdownEditor.jsx
+++ frontend/src/MarkdownEditor.jsx
@@ -29,7 +29,7 @@ const MarkdownEditor = () => {
     await fetch(`${API_URL}/save`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ filename, content: markdown })
+      body: JSON.stringify({ filename, content: markdown }),
     });
     alert("Fichier sauvegardé !");
   };
@@ -45,35 +45,42 @@ const MarkdownEditor = () => {
   };
 
   return (
-    <div className="p-4 max-w-4xl mx-auto">
+    <div>
       <h1 className="text-xl font-bold mb-4">Markdown & Mermaid Editor</h1>
-      <input
-        type="text"
-        className="p-2 border rounded w-full mb-2"
-        placeholder="Nom du fichier"
-        value={filename}
-        onChange={(e) => setFilename(e.target.value)}
-      />
-      <textarea
-        className="w-full h-40 p-2 border rounded"
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-      />
-      <div className="flex gap-2 mt-2">
-        <button className="p-2 bg-blue-500 rounded" onClick={downloadMarkdown}>
-          Télécharger Markdown
-        </button>
-        <button className="p-2 bg-green-500 rounded" onClick={saveMarkdown}>
-          Sauvegarder
-        </button>
-        <button className="p-2 bg-gray-500  rounded" onClick={loadMarkdown}>
-          Charger
-        </button>
+      <div className="flex flex-row">
+        <input
+          type="text"
+          className="p-2 border rounded w-full mb-2"
+          placeholder="Nom du fichier"
+          value={filename}
+          onChange={(e) => setFilename(e.target.value)}
+        />
+        <div className="flex gap-2 mt-2">
+          <button
+            className="p-2 bg-blue-500 rounded"
+            onClick={downloadMarkdown}
+          >
+            Télécharger Markdown
+          </button>
+          <button className="p-2 bg-green-500 rounded" onClick={saveMarkdown}>
+            Sauvegarder
+          </button>
+          <button className="p-2 bg-gray-500  rounded" onClick={loadMarkdown}>
+            Charger
+          </button>
+        </div>
       </div>
-      <div
-        className="mt-4 p-4 border rounded bg-gray-100"
-        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-      />
+      <div className="flex flex-row">
+        <textarea
+          className="basis-1/3 mt-4 p-2 border rounded"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <div
+          className="basis-2/3 mt-4 p-4 border rounded bg-gray-100"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        />
+      </div>
     </div>
   );
 };
